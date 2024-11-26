f86fe221fd844cb3618545f0c2c398682f01011b
xiaenling <xiaenling@canaan-creative.com>
2024-11-26 17:35:11 +0800

    Update ai_poc elf install path.

--- Makefile
+++ Makefile
@@ -7,16 +7,12 @@ dirs := ai_poc
 .PHONY: all clean
 
 all:
-	@rm -rf $(RTT_EXAMPLES_ELF_INSTALL_PATH)/*
-
 ifeq ($(CONFIG_RTT_ENABLE_BUILD_EXAMPLES),y)
 	@$(foreach dir,$(dirs),make -C $(dir) all;)
 endif
 	@echo "Make rtsmart samples done."
 
 clean:
-	@rm -rf $(RTT_EXAMPLES_ELF_INSTALL_PATH)/*
-
 ifeq ($(CONFIG_RTT_ENABLE_BUILD_EXAMPLES),y)
 	@$(foreach dir,$(dirs),make -C $(dir) clean;)
 endif
--- mkenv.mk
+++ mkenv.mk
@@ -8,8 +8,8 @@ export MPP_SRC_DIR := $(SDK_RTSMART_SRC_DIR)/mpp/
 export NNCASE_SRC_DIR := $(SDK_RTSMART_SRC_DIR)/libs/nncase
 export OPENCV_SRC_DIR := $(SDK_RTSMART_SRC_DIR)/libs/opencv
 
-export RTT_EXAMPLES_ELF_INSTALL_PATH := $(SDK_RTSMART_SRC_DIR)/examples/elf/
-$(shell if [ ! -e $(RTT_EXAMPLES_ELF_INSTALL_PATH) ];then mkdir -p $(RTT_EXAMPLES_ELF_INSTALL_PATH); fi)
+export RTT_EXAMPLES_ELF_INSTALL_PATH_AI_POC := $(SDK_RTSMART_SRC_DIR)/examples/elf/ai_poc/
+$(shell if [ ! -e $(RTT_EXAMPLES_ELF_INSTALL_PATH_AI_POC) ];then mkdir -p $(RTT_EXAMPLES_ELF_INSTALL_PATH_AI_POC); fi)
 
 include $(SDK_TOOLS_DIR)/toolchain_rtsmart.mk
 export PATH:=$(CROSS_COMPILE_DIR):$(PATH)
